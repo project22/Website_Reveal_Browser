65adc00cb1f6313e1c41158c1c6b423d7341f0b4
JP Berti <jpberti@yahoo.com>
2014-01-27 13:43:33 -0800

    version 2

--- js/website_reveal_browser.js
+++ js/website_reveal_browser.js
@@ -2,51 +2,70 @@ $(function() {
   
   browserWidth = $(window).width();
 
-  $("iframe").width(browserWidth);
-  
-  $("#view-1").width(browserWidth);
-  $("#view-2").width(0);
+  $("iframe").css( "width", browserWidth );
 
-  $("#sweeper").css({'left': browserWidth});
+  websiteIndex = 0 ;
+  resetFrames();
+
 
   function flipPage (x) {
 
-		$( "#view-1" ).animate({
+		$("#view-1").animate({
 	    width: x,
-	  	}, 500, function() {
-	  });
-	  $( "#view-2" ).animate({
+	  	}, 500);
+	  $("#view-2").animate({
 	    width: browserWidth - x,
-	  	}, 500, function() {
-	  });
+	  	}, 500);
 	  $("#iframe-2").animate({
 	    left:	-x,
-	  	}, 500, function() {
-	  });
+	  	}, 500);
 	  $("#sweeper").animate({
 	    left:	x,
-	  	}, 500, function() {
-	  });
-
+	  	}, 500,function(){ resetFrames() });
   }
 
-  $(window).bind('resize', function() {
-     location.reload();
-	});
+ //  $(window).bind('resize', function() {
+ //     location.reload();
+	// });
 	
+	function resetFrames() {
+		
+		
+		$("#header").html(websites.websites[websiteIndex].name)
+		$("#iframe-1").attr('src', websites.websites[websiteIndex].url);
+  	$("#iframe-2").attr('src', websites.websites[websiteIndex + 1].url);
+  	$('#iframe-1').load(function(){
+  		// alert("done loading");
+      $("#view-1").css( "width", browserWidth );
+	  	$("#view-2").css( "width", 0 );
+	  	$("#iframe-2").css( "left", -browserWidth );
+	  	$("#sweeper").css( "left", browserWidth );    
+	  });
+	  	
+  	websiteIndex += 1;
+	}
 
-  flipPage(browserWidth-(browserWidth*.1))
+  // flipPage(browserWidth-(browserWidth*.1))
 
-	console.log(websites);
+	console.log(websites.websites[0].name);
 
 
   var clicking = false;
 
-	$(window).mousedown(function(){
-	    clicking = true;
-	   	flipPage(event.clientX);
+	// $(window).mousedown(function(){
+	//     clicking = true;
+	//    	flipPage(event.clientX);
 	
-	});
+	// });
+
+	$("#scroll-left").mouseup(function(){
+		flipPage(0);
+		// resetFrames();
+	})
+
+
+
+
 
 	// $(document).mouseup(function(){
 	//     clicking = false;
--- js/websites.js
+++ js/websites.js
@@ -3,5 +3,8 @@ var websites = {"websites": [
 	{name: "Founder Dating", url:"http://jpberti.com/founder_dating_sample/"},
 	{name: "CryptoCurrency Report", url:"http://jpberti.com/ccurrency_angular/"},
 	{name: "W3 Schools", url:"http://www.w3schools.com/"},
+	{name: "CI Surfboards", url:"http://www.cisurfboards.com/"},
+	{name: "Billabong", url:"http://www.billabong.com/"},
+	{name: "Mammoth Mountain", url:"http://www.mammothmountain.com/"},
 
 ]};
