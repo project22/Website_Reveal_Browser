48e59983f822d0fc52c9301ba898607a003b1cb1
JP Berti <jpberti@yahoo.com>
2014-01-29 14:05:49 -0800

    change the sites in json object

--- js/websites.js
+++ js/websites.js
@@ -1,9 +1,11 @@
 
 var websites = {"websites": [
+	{name: "Wind Reports", url:"http://jpberti.com/wind_reports/"},
 	{name: "CryptoCurrency Report", url:"http://jpberti.com/ccurrency_angular/"},
-	{name: "Founder Dating", url:"http://jpberti.com/founder_dating_sample/"},
+	{name: "Founder Dating Mockup", url:"http://jpberti.com/founder_dating_sample/"},
+	{name: "General Assembly", url:"https://generalassemb.ly/"},
+	{name: "Spark Networks", url:"http://www.spark.net/"},
 	{name: "Commission Junction", url:"http://www.cj.com/"},
 	{name: "Impact Radius", url:"http://www.impactradius.com/"},
-	{name: "Spark Networks", url:"http://www.spark.net/"},
-	{name: "General Assembly", url:"https://generalassemb.ly/"},
 ]};
+
